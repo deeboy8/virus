910dcd1ad3a4f1897cef8e48bb922742298c5f20
Demitrus Booker <demitrusw.b@gmail.com>
2024-12-08 14:12:40 -0800

    refactored if-else logic code in main fx to combine heath status being changed to recovered depending on data returned from die or not fx

--- simulate.py
+++ simulate.py
@@ -78,7 +78,7 @@ class Person:
     #     self.sick_days: int = 0 #num of days person is sick
     health_status: HealthStatus = HS.SUSCEPTIBLE
     sick_days: int = 0
-    transmission_rate: float = random.random()
+    transmission_rate: float = random.random() #being treated as a class var -> getting hit once and setting all 12/4
 
     def check_if_infected(self, tprob: float, other_persons: List['Person']) -> bool:
         # iterate over list of random people to simulate interacting with individuals throughout day
@@ -94,9 +94,23 @@ class Person:
             raise ValueError("tprob must be between 0 and 1")
         if len(other_persons) == 0:
             raise IndexError("empty range for other_persons list")
-        person_infected = self.check_if_infected(tprob, other_persons)
+        # person_infected = self.check_if_infected(tprob, other_persons)
 
-        return True if person_infected else False
+        # return True if person_infected else False
+        # return person_infected
+        return self.check_if_infected(tprob, other_persons)
+    
+    def num_sick_days_greater_than_max_sick_days(self, person: 'Person', sickness_factor: float) -> int:
+        # sick_days_randcomization = person.sick_days + 3.0 * sickness_factor
+        return person.sick_days + 3.0 * sickness_factor
+        # if person.sick_days > sick_days_randomization:
+            
+
+    
+    def check_if_survive(self, dprob: float, rand_dprob: float, sickness_factor: float, person: 'Person') -> bool:
+        if rand_dprob < dprob:
+            return True
+        return False
 
     # checks if individual stays sick, dies or gets better
     def die_or_not(self, dprob: float, rand_dprob: float, sickness_factor: float, person: 'Person') -> bool: #, population: List) -> bool:
@@ -108,27 +122,24 @@ class Person:
             raise ValueError("rand_dprob must be between 0 and 1")
         # if len(population) == 0:
         #     raise IndexError("empty range for population")
-        if rand_dprob < dprob:
-            return True
-        else:
-            # else must check if recovers or remains sick
-            sick_days_randomization = person.sick_days + 3.0 * sickness_factor
-            if person.sick_days > sick_days_randomization:
-                print("recovered")
-                return False
+        return self.check_if_survive(dprob, rand_dprob, sickness_factor, person)
+        
          
-def main(tprob: Annotated[float, typer.Argument()], dprob: Annotated[float, typer.Argument()], vprob: Annotated[float, typer.Argument()],
+def main(tprob: Annotated[float, typer.Argument()] = 0.5, dprob: Annotated[float, typer.Argument()] = 0.5, vprob: Annotated[float, typer.Argument()] = 0.5,
                 population_count: Annotated[int, typer.Argument()] = DEFAULT_POPULATION, infected: Annotated[int, typer.Argument()] = DEFAULT_INFECTED_INITIAL,
                 days: Annotated[int, typer.Argument()] = DEFAULT_DAYS):
     nvaccinated = vprob * population_count
-    population: List[Person] = [Person(health_status=HS.INFECTED, sick_days = 1) for _ in range(3)] + [Person() for _ in range(5)] # generate list of Person objects with some set to health_status.INFECTED based on infected value passed on CL
+    # generate list of Person objects with some set to health_status.INFECTED based on infected value passed on CL
+    population: List[Person] = [Person(health_status=HS.INFECTED, sick_days = 1) for _ in range(3)] + [Person(transmission_rate=random.random()) for _ in range(5)] 
     random.shuffle(population)
-    # print(population)
-    # nested loop to iterate over each day with inner loop iterating over each individual determining if gets sick, dies, or ramains the same
+    for i in population:
+        print(i)
+    # nested loop iterating over each day
+    # the inner loop iterates over each individual to determine if person gets sick, gets well, dies, or remains sick and increases day of infection by 1 day
     for day in range(days): # iterate over each day
         for person in population: # check each persons status on each day
             if person.health_status == HS.SUSCEPTIBLE:
-                nexposures: int = random.randint(1, 8)
+                nexposures: int = random.randint(1, 8) # randomly generate int to simulate numer of exposures Person objects encounters for the day
                 other_persons_list: List[Person] = random.sample(population, random.randint(1, min(nexposures, len(population)))) 
                 if person.catch_or_not(tprob, other_persons_list):
                     person.health_status = HS.INFECTED
@@ -137,11 +148,34 @@ def main(tprob: Annotated[float, typer.Argument()], dprob: Annotated[float, type
                 if person.die_or_not(dprob, random.random(), random.random(), person):
                     person.health_status = HS.DEAD
                 else:
-                    person.sick_days += 1
+                    days_sick = person.num_sick_days_greater_than_max_sick_days(person, random.random())
+                    if days_sick > 14:
+                        person.health_status == HS.RECOVERED
+                    else: 
+                        person.sick_days += 1
             elif person.health_status == HS.RECOVERED:
-                continue            
-            else:
-                person.sick_days += 1
+                continue
+            
+            # write count of health_statuses to file for data collection
+                # generate fx to tally up w vars and place in dict: susceptible, recovered, nvaccinated, infected and dead for each day
+                    # add to a pandas dataframe for data collection
+            # switch status to new_health_statuses
+        
+    print('________________________')
+    print('\n')
+    for i in population:
+        print(i)
 
 if __name__ == "__main__":
     typer.run(main)
+
+''' 12/04
+- simulate runs one simulation of n days -> prints out each day
+- what will output be for virus? figure out wha capture from each to output as a whole program
+    - normalize: combine into one format
+- push data to dataframe as opposed to I/O
+    - output to excel and look at data before matplotlib
+
+pydantic
+dataclasses- real python
+'''
