57933327f45af2d7731dc04b350d2a07542f4564
Demitrus Booker <demitrusw.b@gmail.com>
2025-01-08 09:14:10 -0800

    added methods for analyze class

--- simulate.py
+++ simulate.py
@@ -1,14 +1,12 @@
 from enum import Enum
-from typing import List
-# from typing import Self
+from typing import List, Annotated
 from dataclasses import dataclass
 import random
-import typer
-from typing_extensions import Annotated
-import sys
 import pandas as pd
+from typing_extensions import Self
+import sys
+import typer
 import matplotlib.pyplot as plt
-from pydantic import BaseModel, Field
 
 app = typer.Typer()
 
@@ -16,6 +14,9 @@ DEFAULT_POPULATION = 100
 DEFAULT_DAYS = 10
 DEFAULT_INFECTED_INITIAL = 10
 MAX_NEXPOSURES: int = 21
+
+SIMULATE_FILE = 'simulate.csv'
+ANALYZE_FILE = 'analyze.csv'
 
 class HS(int, Enum):
     SUSCEPTIBLE = 0
@@ -26,9 +27,11 @@ class HS(int, Enum):
 
 HealthStatus = HS | int
 
+# class Analyze:
+    # function to import 
+
 class Simulation:
     def __init__(self, population: int, infected: int):
-        self._population = population # should not be touched directly
         self._infected = infected
         # generate a list of Person objects
         self._population: List[Person] = [Person(health_status=HS.INFECTED, sick_days = 1) for _ in range(infected)] + [Person(transmission_rate=random.random()) for _ in range(population - infected)]      
@@ -107,17 +110,17 @@ class Simulation:
         stdv = df[HS.DEAD].std()
         return avg, stdv
     
-    def generate_stats_dict(self):
+    def generate_statistics_dict(self):
         adf_dict = {
             'Trial': 0,
-            'AVG': 0,
+            'AVG_DEATHS': 0,
             'STDV': 0
         }
 
         return adf_dict
-    
-    def run(self, tprob: float, dprob: float, days: int): #, population: List['Person']): 
-        daily_counts= []
+     
+    def run(self, tprob: float, dprob: float, days: int): #TODO: how do we incorporate vaccinated 
+        daily_counts = []
         status_counts: dict = self.health_status_dict()
         df = pd.DataFrame(columns = ['Day', HS.SUSCEPTIBLE, HS.INFECTED, HS.RECOVERED, HS.DEAD, HS.VACCINATED])
         for day in range(days):
@@ -145,46 +148,36 @@ class Simulation:
 
             status_counts['Day'] = day
             df.loc[day] = status_counts
-        # print(df)
+        
         return df
 
-# @dataclass
-class Person(BaseModel):
+@dataclass
+class Person:
     MAX_SICK_DAYS: int = 14
-    
-    # def __init__(self, health_status: HealthStatus = HealthStatus.SUSCEPTIBLE, sick_days: int = 0):
-    #     self.health_status: HealthStatus = health_status
-    #     self.sick_days: int = 0 #num of days person is sick
     health_status: HealthStatus = HS.SUSCEPTIBLE
     sick_days: int = 0
-    # sick_days: int = Field(0, gt=0, le=1)
-    transmission_rate: float = random.random() #being treated as a class var -> getting hit once and setting all 12/4
-
+    transmission_rate: float = random.random() 
+    
     def check_if_infected(self, tprob: float, other_persons: List['Person']) -> bool:
+        if (tprob < 0 and tprob > 1):
+            raise ValueError("tprob must be between 0 and 1")
         # iterate over list of random people to simulate interacting with individuals throughout day
         for other_person in other_persons:
             if other_person.health_status == HS.INFECTED: 
-                # if Person.transmission_rate < tprob:
                 if self.transmission_rate < tprob:
                     return True
                 
         return False
 
-    def catch_or_not(self, tprob: float, other_persons: List['Person']) -> bool: 
-        if tprob < 0 or tprob > 1:
+    def catch_or_not(self, tprob: float, other_persons: List[Self]) -> bool: 
+        if (tprob < 0 and tprob > 1):
             raise ValueError("tprob must be between 0 and 1")
-        if len(other_persons) == 0:
-            raise IndexError("empty range for other_persons list")
-        # person_infected = self.check_if_infected(tprob, other_persons)
-
-        # return True if person_infected else False
-        # return person_infected
         return self.check_if_infected(tprob, other_persons)
     
     def num_sick_days_greater_than_max_sick_days(self, person: 'Person', sickness_factor: float) -> int:
-        # sick_days_randcomization = person.sick_days + 3.0 * sickness_factor
+        if (sickness_factor < 0 or sickness_factor > 1):      
+            raise ValueError("sickness_factor must be between 0 and 1")
         return person.sick_days + 3.0 * sickness_factor
-        # if person.sick_days > sick_days_randomization:
 
     def check_if_survive(self, dprob: float, rand_dprob: float, sickness_factor: float, person: 'Person') -> bool:
         if rand_dprob < dprob:
@@ -200,55 +193,62 @@ class Person(BaseModel):
             raise ValueError("dprob must be between 0 and 1")
         if (rand_dprob < 0 or rand_dprob > 1):
             raise ValueError("rand_dprob must be between 0 and 1")
-        # if len(population) == 0:
-        #     raise IndexError("empty range for population")
+        
         return self.check_if_survive(dprob, rand_dprob, sickness_factor, person)
 
 @app.command()
-def visualize():
-    pass
+# def visualize(dmin: int, dmax: int, input_file: __file__, output_file: __file__):
+    # create a visualize object
+    # 
+    # pass
 
-# simulate n simulations
+# simulate n number of simulations
 @app.command()
 def analyze(nsimulations: Annotated[int, typer.Argument],
             tprob: Annotated[float, typer.Argument()] = 0.05, 
-            dprob: Annotated[float, typer.Argument()] = 0.5,
+            dprob: Annotated[float, typer.Argument()] = 0.05,
             days: Annotated[int, typer.Argument()] = DEFAULT_DAYS,
             infected: Annotated[int, typer.Argument()] = DEFAULT_INFECTED_INITIAL,
-            population_count: Annotated[int, typer.Argument()] = DEFAULT_POPULATION): # concerned with writing down the avg deaths for each trial run
-    adf = pd.DataFrame(columns = ['AVG', 'STDV'])
+            population_count: Annotated[int, typer.Argument()] = DEFAULT_POPULATION): 
+    adf = pd.DataFrame(columns = ['AVG_DEATHS', 'STDV'])
     for trial in range(nsimulations):
         sim = Simulation(population_count, infected)
-        adf_dict = sim.generate_stats_dict()
+        adf_dict = sim.generate_statistics_dict()
         df = sim.run(tprob, dprob, days)
-        adf_dict['AVG'], adf_dict['STDV'] = sim.calculate_stats(df)
+        adf_dict['AVG_DEATHS'], adf_dict['STDV'] = sim.calculate_stats(df)
         adf_dict['Trial'] = trial
         adf.loc[trial + 1] = adf_dict
-    print(adf)
-    sim.write_values_to_file(adf, 'analyze.csv')
+    print(adf) #TODO: remove
+    sim.write_values_to_file(adf, ANALYZE_FILE)
 
 # run a single simulation
 @app.command()
 def simulate(tprob: Annotated[float, typer.Argument()] = 0.05, 
-             dprob: Annotated[float, typer.Argument()] = 0.5,
+             dprob: Annotated[float, typer.Argument()] = 0.05,
+             vaccinated: Annotated[int, typer.Argument()] = 0,
              infected: Annotated[int, typer.Argument()] = DEFAULT_INFECTED_INITIAL,
              days: Annotated[int, typer.Argument()] = DEFAULT_DAYS,
-             population_count: Annotated[int, typer.Argument()] = DEFAULT_POPULATION): 
+             population_count: Annotated[int, typer.Argument()] = DEFAULT_POPULATION):
+            #  output_file: Annotated[__file__, typer.Argument()] = SIMULATE_FILE): 
     sim = Simulation(population_count, infected)
     df = sim.run(tprob, dprob, days)
-    print(df)
-    sim.write_values_to_file(df, 'simulate.csv')
+    print(df) #TODO: remove
+    sim.write_values_to_file(df, SIMULATE_FILE)
+    value = df.iloc[8, 4]
+    print(f"Populaiton: {population_count}")
+    print(f"Vaccination Probability: {vaccinated}")
+    print(f"Transmission Probability: {tprob}")
+    print(f"Initial Infections: {infected}")
+    print(f"Siumulation Period: {days}")
+    print(f"Number of Recovered: {df.iloc[8, 4]}") #TODO: get value but not df
+    print(f"Number of Dead: {df[HS.DEAD]}") #TODO: get value but not df
+    print(f"Case Fatality Rate: {df[HS.DEAD]/df[HS.RECOVERED]}") #TODO: get value but not df
 
 if __name__ == "__main__":
     app()
-    
+
 
 '''
-To Do:
-    - pydantic
-    - unit tests
-
-
 inputs/outputs:
     - program 1
         - input: tprob, dprob, vprob, pop count, infected, days
